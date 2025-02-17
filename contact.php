068f7280cbb9d4136a9c9d1354aafedc2b3819ca
Kotesh Rekha <156227398+KoteshRekha@users.noreply.github.com>
2025-02-16 22:35:23 -0600

    Code Updated

--- contact.php
+++ contact.php
@@ -31,7 +31,7 @@ if(isset($_POST['send'])){
       $insert_message = $conn->prepare("INSERT INTO `message`(user_id, name, email, number, message) VALUES(?,?,?,?,?)");
       $insert_message->execute([$user_id, $name, $email, $number, $msg]);
 
-      $message[] = 'sent message successfully!';
+      $message[] = 'Message Sent Successfully!';
 
    }
 
@@ -45,7 +45,7 @@ if(isset($_POST['send'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>contact</title>
+   <title>Contact</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -60,13 +60,13 @@ if(isset($_POST['send'])){
 
 <section class="contact">
 
-   <h1 class="title">get in touch</h1>
+   <h1 class="title">Get in Touch</h1>
 
    <form action="" method="POST">
-      <input type="text" name="name" class="box" required placeholder="enter your name">
-      <input type="email" name="email" class="box" required placeholder="enter your email">
-      <input type="number" name="number" min="0" class="box" required placeholder="enter your number">
-      <textarea name="msg" class="box" required placeholder="enter your message" cols="30" rows="10"></textarea>
+      <input type="text" name="name" class="box" required placeholder="Enter your Name">
+      <input type="email" name="email" class="box" required placeholder="Enter your Email">
+      <input type="number" name="number" min="0" class="box" required placeholder="Enter your Number">
+      <textarea name="msg" class="box" required placeholder="Enter your Message" cols="30" rows="10"></textarea>
       <input type="submit" value="send message" class="btn" name="send">
    </form>
 
